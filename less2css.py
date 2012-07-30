661eee9743b1be48c8805e8b83e71acf3d2be82d
timdouglas <github@timdouglas.co.uk>
2012-07-30 15:38:39 +0100

    initial commit

---
+++ less2css.py
@@ -0,0 +1,81 @@
+import sublime, sublime_plugin
+import subprocess
+import re
+
+#define methods to convert css, either the current file or all
+class LessToCss:
+  def __init__(self, view):
+    self.view = view
+
+  def convertOne(self):
+    #get the current file & its css variant
+    fn = self.view.file_name().encode("utf_8")
+    fn_css = re.sub('\.less', '.css', fn)
+    cmd = ["ae-lessc", fn, fn_css] #proxy for `lessc fn > fn_css`
+
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr=subprocess.PIPE)
+
+    while True:
+      try:
+        stdout, stderr = p.communicate()
+      except ValueError:
+        break
+      #remove control characters  
+      out = stderr.decode("ascii")
+      out = re.sub('\033\[[^m]*m', '', out)
+
+    return out
+
+  def convertAll(self):
+    #call generic *.less > *.css command
+    p = subprocess.Popen('ae-less2css', stdout = subprocess.PIPE, stderr=subprocess.PIPE)
+
+    while True:
+      try:
+        stdout, stderr = p.communicate()
+      except ValueError:
+        break
+      #remove control characters  
+      out = stderr.decode("ascii")
+      out = re.sub('\033\[[^m]*m', '', out)
+
+    return out  
+
+
+
+
+############################
+##### SUBLIME COMMANDS #####
+############################
+
+#single less file
+class LessToCssCommand(sublime_plugin.TextCommand):
+  def run(self, text):
+    l2c = LessToCss(self.view)
+    fn = self.view.file_name().encode("utf_8")
+    
+    if fn.find(".less") > -1:
+      sublime.status_message("Compiling .less files...")
+      resp = l2c.convertOne()
+
+      if resp != "":
+        sublime.error_message(resp)
+      else:
+        sublime.status_message(".less file compiled successfully")
+
+#all less files
+class AllLessToCssCommand(sublime_plugin.TextCommand):
+  def run(self, text):
+    l2c = LessToCss(self.view)
+    sublime.status_message("Compiling .less files...")
+    resp = l2c.convertAll()
+
+    if resp != "":
+      sublime.error_message(resp)
+    else:
+      sublime.message_box("All .less files compiled successfully")
+
+#listener to current less file
+class LessToCssSave(sublime_plugin.EventListener):
+  def on_post_save(self, view):
+    view.run_command("less_to_css")
